f6504e145dc079bd05f1d4a8b6525958193fd555
Fusion Chen <fusion_c@outlook.com>
2018-05-31 04:14:24 +0800

    add ip function

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ from linebot.exceptions import (
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup as bs
+import requests
+import re
 
 app = Flask(__name__)
 
@@ -33,15 +35,46 @@ def callback():
 
     return 'OK'
 
+def checkIPflow(ip : str):
+    payload = {
+        "ip": ip,
+        "submit": "Submit"
+    }
+
+    res = requests.post("https://uncia.cc.ncu.edu.tw/dormnet/index.php?section=netflow&sub=24hr", data=payload)
+    res.encoding = "big5"
+    soup = bs(res.text, "lxml")
+
+    taglist1 = soup.find_all('tr', attrs={'bgcolor': "#ffffbb"})
+    l1 = []
+    for trtag in taglist1:
+        s1 = trtag.text
+        l1 += re.findall("\(.+\)", s1)
+
+    taglist2 = soup.find_all('tr', attrs={'bgcolor': "#bbbbff"})
+    l2 = []
+    for trtag in taglist2:
+        s2 = trtag.text.strip().split("\n")
+        l2 = s2
+
+    l3 = zip(l2[1:], l1)
+    content = "24hr 總量"
+    for a,b in l3:
+        content += '{}:\t\t{}\n'.format(a, b)
+    return  content
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     print("Handle: reply_token: " + event.reply_token + ", message: " + event.message.text)
-    content = "{}: {}".format(event.source.user_id, event.message.text)
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=content))
+    if re.match("((?:(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d)))\.){3}(?:25[0-5]|2[0-4]\d|((1\d{2})|([1-9]?\d))))",
+                event.message.text) :
+        ip = event.message.text
+        content = checkIPflow(ip)
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content))
+        return 0
 
 import os
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=os.environ['PORT'])
+    app.run(host='0.0.0.0', port=os.environ['PORT'])
